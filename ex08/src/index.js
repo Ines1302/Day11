f2730d44322542da33cef0a4992a23a13722c213
Ines1302 <seeking.knowledge13@gmail.com>
2021-03-19 10:33:03 +0100

    Create index.js

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,14 +1,26 @@
 function myMutation(arr) {
-  newArr1 = Array.from(arr[0]);
-  newArr2 = Array.from(arr[1]);
-
-  if (newArr1.contains(newArr2)){
-      return true;
-  }
-  return false;
-
+    arr1 = arr[0].toLowerCase();
+    arr2 = arr[1].toLowerCase();
   
-}
+    for (var i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) === -1) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-console.log(myMutation(["Hello", "Women"]));
+
+console.log(myMutation(["hello", "hey"]));
+console.log(myMutation(["hello", "Hello"]));
+console.log(myMutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+console.log(myMutation(["Mary", "Army"]));
+console.log(myMutation(["Mary", "Aarmy"]));
+console.log(myMutation(["Alien", "line"]));
+console.log(myMutation(["floor", "for"]));
+console.log(myMutation(["hello", "neo"]));
+console.log(myMutation(["voodoo", "no"]));
+console.log(myMutation(["ate", "date"]));
+console.log(myMutation(["Tiger", "Zebra"]));
+console.log(myMutation(["Noel", "Ole"]));
 module.exports = myMutation;
